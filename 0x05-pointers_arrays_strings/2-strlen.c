fc072ee29d65cff6902dafd349c563f2fda33b7a
Renny Yegon <cliveclowes5@gmail.com>
2023-07-24 02:42:36 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of the string
+ * @s:-string to be checked
+ * Return:The length of the string
+ */
+
+int _strlen(char *s)
+{
+
+int length = 0;
+
+	while (s[length])
+		length++;
+	return (length);
+}
