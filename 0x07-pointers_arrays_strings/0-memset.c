44d2881be77649567dc20b05cdbb08a66f19aad5
Renny Yegon <cliveclowes5@gmail.com>
2023-07-31 11:26:35 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,17 +11,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i = 0;
 
-    for (i = 0; i < n; i++)
-    {
-        if (s + i == NULL)
-        {
-            break;
-        }
-        s[i] = b;
-    }
+	while (i < n)
+	{
+	s[i] = b;
+	i++;
+	}
 
-    return s;
+	return (s);
 }
-
