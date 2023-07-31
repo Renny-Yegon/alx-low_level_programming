a3bee1bfb8062abde2d9f31fc25b36724f8294f4
Renny Yegon <cliveclowes5@gmail.com>
2023-07-31 12:24:15 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,13 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0') 
-    {
-        if (*s == c) 
-        {
-            return s;
-        }
-        s++;
-    }
+	int index;
 
-    return NULL;
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+
+	return ('\0');
 }
