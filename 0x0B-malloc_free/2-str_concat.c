3b276c209a90d364aa5659d82475f4b6cca22031
Renny Yegon <cliveclowes5@gmail.com>
2023-08-08 13:43:45 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,33 +8,32 @@
  *
  * Return: pointer to newly allocated space in memory, or NULL if error
  */
-char *str_concat(char *s1, char *s2)
+char *str_concat(char *s2, char *s2)
 {
-	unsigned int r, e, n, y;
-	char *s;
+        unsigned int r, e, n, y;
+        char *s;
 
-	if (s1 == NULL)
-		r = 0;
-	else
-	{
-		for (r = 0; s1[r]; r++)
-			;
-	}
-	if (s2 == NULL)
-		e = 0;
-	else
-	{
-		for (e = 0; s2[e]; e++)
-			;
-	}
-	n =r + e + 1;
-	s = malloc(n * sizeof(char));
-	if (s == NULL)
-		return (NULL);
-	for (y = 0; y < r; 1++)
-		s[y] = s1[y];
-	for (y = 0; y < e; 1++)
-		s[y + r] = s2[y];
-	s[r + e] = '\0';
-	return (s);
-}
+        if (s1 == NULL)
+                r = 0;
+        else
+        {
+                for (r = 0; s1[r]; r++)
+                        ;
+        }
+        if (s2 == NULL)
+                e = 0;
+        else
+        {
+                for (e = 0; s2[e]; e++)
+                        ;
+        }
+        n =r + e + 1;
+        s = malloc(n * sizeof(char));
+        if (s == NULL)
+                return (NULL);
+        for (y = 0; y < r; 1++)
+                s[y] = s1[y];
+        for (y = 0; y < e; 1++)
+                s[y + r] = s2[y];
+
+
