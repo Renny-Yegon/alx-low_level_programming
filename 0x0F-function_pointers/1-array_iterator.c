108825e99650385e494a92bad6f2ce3150d40b85
Renny Yegon <cliveclowes5@gmail.com>
2023-08-16 14:55:13 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,8 +2,8 @@
 
 /**
  * array_iterator - xecutes a function given as a parameter on each
- * element of an array
- * @array: array of integer
+ * element of an array.
+ * @array: array of intege
  * @size: size of array
  * @action: function pointer
  */
